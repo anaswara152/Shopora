8f2d5dbedb171eb3a7cace3ed7985789e8a728e6
anaswara152 <anaswarat93@gmail.com>
2026-05-02 14:24:48 +0530

    fixed database config

--- shopora/settings.py
+++ shopora/settings.py
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'shopora.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=False
     )
 }
 # Password validation
--- user/apps.py
+++ user/apps.py
@@ -5,12 +5,15 @@ class UserConfig(AppConfig):
     name = 'user'
 
     def ready(self):
-        import os
-        if os.environ.get('RENDER') == 'TRUE':
-            from django.contrib.auth.models import User
+        from django.db.models.signals import post_migrate
+        from django.contrib.auth.models import User
+
+        def create_admin(sender, **kwargs):
             if not User.objects.filter(username='admin').exists():
                 User.objects.create_superuser(
                     username='admin',
                     email='admin@gmail.com',
                     password='admin'
                 )
+
+        post_migrate.connect(create_admin, sender=self)
